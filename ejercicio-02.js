e3cfa5760bfed99aeacb865db89272bace4e8f08
blue-keyboard <bluekeyboard9@gmail.com>
2024-05-24 00:35:57 +0200

    add  exercise 2

---
+++ ejercicio-02.js
@@ -0,0 +1,17 @@
+const API_URL = 'https://pokeapi.co/api/v2/pokemon/'
+const img = document.querySelector('.random-image')
+
+const getPokemonFromApi = async (id) => {
+   const res = await fetch(API_URL + id)
+   const response = await res.json()
+   return response
+}
+
+const displayRandomPokemonImage = async () => {
+   const randomId = Math.floor(Math.random() * (151 + 1)) + 1
+   const pokemon = await getPokemonFromApi(randomId)
+   const pokemonImageUrl = pokemon.sprites.other.home.front_default
+   img.src = pokemonImageUrl
+}
+
+displayRandomPokemonImage()
